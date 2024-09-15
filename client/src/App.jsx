a4ea994ec603348579959c31e91efb5f7c272995
AKSAR <akshatjaiswal2518@gmail.com>
2024-09-15 16:20:59 +0530

    initial setup complete

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Let's Start</h1>
+    </>
+  );
+}
+
+export default App;
