8b007029fb075ffab5f27335e79ccc1848b08241
AKSAR <akshatjaiswal2518@gmail.com>
2024-10-07 21:27:32 +0530

    add default avatar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import avatar_black from "../assets/images/avatar_black.png";
+import avatar_white from "../assets/images/avatar_white.png";
 
 const Navbar = ({ toggleSidebar }) => {
+  const isDarkTheme = document.documentElement.classList.contains("dark");
+
   return (
     <div
       className="flex justify-between items-center z-10 p-4 shadow-md fixed top-0 left-0 right-0 bg-gray-100 dark:bg-gray-800"
@@ -16,7 +20,13 @@ const Navbar = ({ toggleSidebar }) => {
       <Link to="/">
         <h1 className="text-xl font-semibold cursor-pointer">VidFlow</h1>
       </Link>
-      <div className="w-8 h-8 rounded-full cursor-pointer bg-gray-300 dark:bg-gray-700"></div>
+      <div className="w-8 h-8 rounded-full cursor-pointer">
+        <img
+          className="h-full w-full"
+          src={isDarkTheme ? avatar_white : avatar_black}
+          alt="avatar"
+        />
+      </div>
     </div>
   );
 };
