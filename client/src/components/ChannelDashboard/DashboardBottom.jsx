75350389a62cb80ced8490e74ce9837a87616652
AKSAR <akshatjaiswal2518@gmail.com>
2024-10-14 17:08:28 +0530

    fix dashboard

--- client/src/components/ChannelDashboard/DashboardBottom.jsx
+++ client/src/components/ChannelDashboard/DashboardBottom.jsx
@@ -6,9 +6,25 @@ const DashboardBottom = ({ tabSelected }) => {
   return (
     <div className="p-6 w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       {tabSelected === "videos" ? (
-        <VideoCard isChannelDashboard={true} />
+        <>
+          <VideoCard isChannelDashboard={true} />
+          <VideoCard isChannelDashboard={true} />
+          <VideoCard isChannelDashboard={true} />
+          <VideoCard isChannelDashboard={true} />
+          <VideoCard isChannelDashboard={true} />
+          <VideoCard isChannelDashboard={true} />
+          <VideoCard isChannelDashboard={true} />
+          <VideoCard isChannelDashboard={true} />
+        </>
       ) : (
-        <Playlist title="Playlist 1" />
+        <>
+          <Playlist title="Playlist 1" />
+          <Playlist title="Playlist 2" />
+          <Playlist title="Playlist 3" />
+          <Playlist title="Playlist 4" />
+          <Playlist title="Playlist 5" />
+          <Playlist title="Playlist 6" />
+        </>
       )}
     </div>
   );
--- client/src/components/ChannelDashboard/DashboardTop.jsx
+++ client/src/components/ChannelDashboard/DashboardTop.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
 const DashboardTop = () => {
-  const [isOwner, setIsOwner] = useState(false);
+  const [isOwner, setIsOwner] = useState(true);
+  const [isSubscribed, setIsSubscribed] = useState(false);
 
   return (
     <div className="p-6 max-w-4xl mx-auto">
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col sm:flex-row gap-6 sm:gap-0 items-center justify-between">
         <div className="flex items-center space-x-4">
           <div className="w-16 h-16 rounded-full bg-gray-300 dark:bg-gray-700" />
           <div>
@@ -13,16 +14,34 @@ const DashboardTop = () => {
             <p>100K subscribers · 100 videos</p>
           </div>
         </div>
-        {/* <button className="bg-red-500 text-white px-4 py-2 rounded-lg">
-          Subscribe
-        </button> */}
-        <button
+        {/* <button
           className={`${
             isOwner ? "bg-blue-500" : "bg-red-500"
           } text-white transition-all duration-300 active:scale-95 px-4 py-2 rounded-lg`}
         >
           {isOwner ? "Upload" : "Subscribe"}
-        </button>
+        </button> */}
+        {!isOwner && !isSubscribed && (
+          <button
+            className="bg-red-500 text-white transition-all duration-300 active:scale-95 px-4 py-2 rounded-lg"
+            onClick={() => setIsSubscribed(true)}
+          >
+            Subscribe
+          </button>
+        )}
+        {!isOwner && isSubscribed && (
+          <button
+            className="bg-gray-500 text-white transition-all duration-300 active:scale-95 px-4 py-2 rounded-lg"
+            onClick={() => setIsSubscribed(false)}
+          >
+            Unsubscribe
+          </button>
+        )}
+        {isOwner && (
+          <button className="bg-blue-500 text-white transition-all duration-300 active:scale-95 px-4 py-2 rounded-lg">
+            Upload
+          </button>
+        )}
       </div>
     </div>
   );
