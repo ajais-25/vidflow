35d8d83358cde85d1395211683b803032a78c740
AKSAR <akshatjaiswal2518@gmail.com>
2025-01-03 18:08:13 +0530

    add lazy loading

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,40 +1,128 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { lazy, Suspense } from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import Layout from "./Layout.jsx";
-import Home from "./pages/Home.jsx";
-import Subscriptions from "./pages/Subscriptions.jsx";
-import WatchHistory from "./pages/WatchHistory.jsx";
-import Playlists from "./pages/Playlists.jsx";
-import NotFound from "./pages/NotFound.jsx";
-import PlaylistIndividual from "./pages/PlaylistIndividual.jsx";
-import ChannelDashboard from "./pages/ChannelDashboard.jsx";
-import Upload from "./pages/Upload.jsx";
-import VideoPage from "./pages/VideoPage.jsx";
-import Login from "./pages/Login.jsx";
-import SignUp from "./pages/SignUp.jsx";
+const Layout = lazy(() => import("./Layout.jsx"));
+const Home = lazy(() => import("./pages/Home.jsx"));
+const Subscriptions = lazy(() => import("./pages/Subscriptions.jsx"));
+const WatchHistory = lazy(() => import("./pages/WatchHistory.jsx"));
+const Playlists = lazy(() => import("./pages/Playlists.jsx"));
+const NotFound = lazy(() => import("./pages/NotFound.jsx"));
+const PlaylistIndividual = lazy(() => import("./pages/PlaylistIndividual.jsx"));
+const ChannelDashboard = lazy(() => import("./pages/ChannelDashboard.jsx"));
+const Upload = lazy(() => import("./pages/Upload.jsx"));
+const VideoPage = lazy(() => import("./pages/VideoPage.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const SignUp = lazy(() => import("./pages/SignUp.jsx"));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<SignUp />} />
-      <Route path="upload" element={<Upload />} />
-      <Route path="" element={<Home />} />
-      <Route path="dashboard" element={<ChannelDashboard />} />
-      <Route path="history" element={<WatchHistory />} />
-      <Route path="playlists" element={<Playlists />} />
-      <Route path="playlists/:id" element={<PlaylistIndividual />} />
-      <Route path="subscriptions" element={<Subscriptions />} />
-      {/* Video */}
-      <Route path="video/:id" element={<VideoPage />} />
-      <Route path="*" element={<NotFound />} />
+    <Route>
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Layout />
+          </Suspense>
+        }
+      >
+        <Route
+          path="login"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Login />
+            </Suspense>
+          }
+        />
+        <Route
+          path="register"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <SignUp />
+            </Suspense>
+          }
+        />
+        <Route
+          path="upload"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Upload />
+            </Suspense>
+          }
+        />
+        <Route
+          path=""
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="dashboard"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ChannelDashboard />
+            </Suspense>
+          }
+        />
+        <Route
+          path="history"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <WatchHistory />
+            </Suspense>
+          }
+        />
+        <Route
+          path="subscriptions"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Subscriptions />
+            </Suspense>
+          }
+        />
+        {/* Playlist */}
+        <Route
+          path="playlists"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Playlists />
+            </Suspense>
+          }
+        />
+        <Route
+          path="playlists/:id"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <PlaylistIndividual />
+            </Suspense>
+          }
+        />
+        {/* Video */}
+        <Route
+          path="video/:id"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <VideoPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
+      </Route>
     </Route>
   )
 );
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don't have an account yet?{" "}
                 <Link
-                  to="/signup"
+                  to="/register"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Sign up
