8245b067fd431a6537519ec922af38efcaa9712d
AKSAR <akshatjaiswal2518@gmail.com>
2025-01-06 20:33:40 +0530

    fix in subscription

--- client/src/components/SubscriptionCard.jsx
+++ client/src/components/SubscriptionCard.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SubscriptionCard = ({ channel }) => {
   return (
-    <div className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md flex flex-col items-center space-y-2 active:scale-95 md:hover:scale-105 cursor-pointer transition-all duration-300">
+    <Link
+      to={`/c/${channel?.username}`}
+      className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md flex flex-col items-center space-y-2 active:scale-95 md:hover:scale-105 cursor-pointer transition-all duration-300"
+    >
       <div className="w-16 h-16 bg-gray-300 dark:bg-gray-700 rounded-full">
         <img
           src={channel?.avatar}
@@ -16,7 +20,7 @@ const SubscriptionCard = ({ channel }) => {
       <p className="text-gray-500 dark:text-gray-400 text-sm">
         @{channel?.username}
       </p>
-    </div>
+    </Link>
   );
 };
 
