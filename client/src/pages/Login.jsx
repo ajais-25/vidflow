5ff00218fffe105a3437cff6fd029e991a52d778
AKSAR <akshatjaiswal2518@gmail.com>
2025-01-19 00:20:20 +0530

    style: enhance layout for Login and SignUp pages with full-screen centering

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="h-screen flex justify-center items-center overflow-y-hidden bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
