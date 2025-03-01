0be3ef860e406694f8d7017f40da7a143935b6db
AKSAR <akshatjaiswal2518@gmail.com>
2025-03-02 00:52:52 +0530

    add backend url to config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/v1": "http://localhost:3000",
+      "/api/v1": import.meta.env.VITE_API_URL,
     },
   },
 });
