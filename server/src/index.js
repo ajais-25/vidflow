321aca3348c3e814ccc49fd6197e322e796eb632
AKSAR <akshatjaiswal2518@gmail.com>
2025-03-02 01:08:11 +0530

    add render code

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,29 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import connectDB from "./db/connect.js";
 import { app } from "./app.js";
+import axios from "axios";
+
+// for render
+const url = `${process.env.RENDER_URL}`;
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+    axios
+        .get(url)
+        .then((response) => {
+            console.log(
+                `Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`
+            );
+        })
+        .catch((error) => {
+            console.error(
+                `Error reloading at ${new Date().toISOString()}:`,
+                error.message
+            );
+        });
+}
+
+setInterval(reloadWebsite, interval);
 
 connectDB()
     .then(() => {
